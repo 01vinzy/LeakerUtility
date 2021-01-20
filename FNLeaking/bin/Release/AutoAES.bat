ca5fe0c6ce4533ae40eb2b46f15c660e7eb7fca9
PeQuLeaks <72738082+PeQuLeaks@users.noreply.github.com>
2021-01-20 18:03:39 +0100

    Delete FNLeaking directory

--- FNLeaking/bin/Release/AutoAES.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-bitsadmin.exe /transfer "AES" "https://lucas7yoshi.github.io/aes.html" "%cd%\AES.txt" >nul
-set /p aes=<AES.txt
--- FNLeaking/newmap.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-echo Generating New Map...
-bitsadmin.exe /transfer "map" "https://media.fortniteapi.io/images/map.png?showPOI=false&lang=eng" "%cd%\map-poi.png" >nul
-set /p map=<map-poi.png
-echo Succesfully Generated New Map!
